261d9638ff2434ba4a9a9a03a45308a7bf04f04f
John Simiyu <jonnyeinstein@gmail.com>
2022-05-27 02:46:46 +0300

    flask api to return matrix operations

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
+import csv
+from doctest import debug
 from flask import Flask, request
-from werkzeug import secure_filename
+from matrix import Matrix
+# from werkzeug import secure_filename
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/echo', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
       f = request.files['file']
-    #   f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+      # open file and read its contents
+      data = []
+      
+      # read csv file
+      with open(f.filename, 'r') as obj:
+          # Return a reader object which will
+          # iterate over lines in the given csvfile
+          csv_reader = csv.reader(obj)
+          
+          # convert string to list
+          data = list(csv_reader)
+      # perform all matrix operations
+      matrix = Matrix(data)
+      
+      # perform all matrix contents and join them as a string
+      result = "*********** Matrix to String ***********\n" + matrix.matrixToString() + '\n' + "*********** Flatten Matrix ***********\n" + matrix.matrixFlatten() + '\n' + "*********** Inverted Matrix ***********\n" + matrix.matrixInvert() + '\n' + "*********** Product of all Elements ***********\n" + str(matrix.matrixProduct()) + '\n' + "*********** Sum of all Elements ***********\n" + str(matrix.matrixSum())
+      
+      return result
     return 'Hello, please upload a csv file!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
